ba492b58341866af7b28457971b084e1de3e8cb4
cpchristensen <112569319+cpchristensen615@users.noreply.github.com>
2022-10-20 03:17:06 +0000

    everything  works

--- main.cpp
+++ main.cpp
@@ -20,8 +20,14 @@ int main() {
                            {4, 64, 74, 84, 94},
                            {5, 94, 84, 74, 64} };
 
-  
-  
+  for (int row = 5; row < nRows; row++){
+    for (int col = 1; col < 5; col++){
+      int temp;
+      cin >> temp;
+      grades[row][0] = (row + 1);
+      grades[row][col] = temp;
+    }
+  } 
 
 
 
